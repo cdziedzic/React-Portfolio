8dfcd24eca234883988325ed63f2d6de1639fcd5
chris dziedzic <cdziedzic@gmail.com>
2023-09-25 16:21:57 -0400

    added html for resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,22 +1,26 @@
 export default function Resume() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <h1>Resume</h1>
+      <h2>Download my <a href="">Resume</a></h2>
+      <ul> Front-end Proficienicies</ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>JQuery</li>
+        <li>Responsive Design</li>
+        <li>React</li>
+
+        
+      <ul>Back-end Proficiencies</ul>  
+        <li>APIs</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MySql, Seqeulize</li>
+        <li>MongoDb, Mongoose</li>
+        <li>REST</li>
+        <li>GraphQL</li>
+
     </div>
   );
 }
