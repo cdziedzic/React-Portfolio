d65a06abe5b5b2e69ad9229326c229c3f1a5beb9
chris dziedzic <cdziedzic@gmail.com>
2023-09-26 15:25:42 -0400

    added several state variables to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,34 +1,93 @@
+import { useState } from "react";
+
 export default function Contact() {
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState('');
-  const [otherField, setOtherField] = useState('');
+  const [messageField, setMessageField] = useState('');
+  const [messageError, setMessageError] = useState('')
+  const [isMessageActive, setIsMessageActive] = useState(false)
+  const onFocus = () => setIsMessageActive(true)
+const onBlur = () => setIsMessageActive(false)
 
+  // Function to handle email input and validation
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+
+    if (!value) {
+      setEmailError('E-mail is required');
+    }
+    else if (!isValidEmail(value)) {
+      setEmailError('Invalid email address');
+    } else {
+      setEmailError('');
+    }
+  };
+  const handleMessageChange = (event) => {
+    const value = event.target.value;
+    setMessageField(value);
+      if (!value) {
+        setMessageError('Message is required');
+      }
+      else {
+        setMessageError('');
+      }
+    
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Additional validation logic for other fields can be added here
+
+    // If email is valid, you can proceed with form submission
+    if (isValidEmail(email)) {
+      // Handle form submission here
+      console.log('Form submitted successfully');
+    } else {
+      setEmailError('Invalid email address');
+    }
+  };
+ 
+  // Function to check if the email address is valid
+  const isValidEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  };
 
 
   return (
     <div>
-
-      <h1>Contact Page</h1>
-      <label htmlFor="">Name:</label>
-      <div class="input-group mb-3">
-        <div class="input-group-prepend">
-        </div>
-        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" />
-      </div>
-        <label htmlFor="">E-mail Address:</label>
-      <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Please enter your e-mail address" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-        <div class="input-group-append">
-        </div>
-      </div>
-      <label htmlFor="">Message:</label>
-      <div class="input-group">
-        <div class="input-group-prepend">
-
-        </div>
-        <textarea class="form-control" aria-label="With textarea"></textarea>
-      </div>
-        <button>Submit</button>
+      <h1>Form Validation</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email Address:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <span style={{ color: 'red' }}>{emailError}</span>
+        <br />
+        <label htmlFor="messageField">Message:</label>
+        <p>Message Field is Active: {isMessageActive ? 'Yes' : 'No'}</p>
+        <input
+          type="text"
+          id="messageField"
+          name="messageField"
+          onBlur={onBlur}
+          onFocus={onFocus}
+          value={messageField}
+          onChange={handleMessageChange}
+        />
+        <span style={{ color: 'red' }}>{messageError}</span>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+     
     </div>
   );
 }
+
