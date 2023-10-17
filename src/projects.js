ed60bb18f6dd810bec99735bd38ad2be7b11c017
chris dziedzic <cdziedzic@gmail.com>
2023-10-17 11:44:01 -0400

    updated projects

--- src/projects.js
+++ src/projects.js
@@ -8,35 +8,45 @@ const projects = [
         description: 'A website that pulls images from NASA api and pairs it with a quote from a quote api'
     },
     {
+        title: 'Accolade',
+        github: 'https://github.com/danmac121/GameTracker',
+        deployed: 'https://whispering-gorge-83170-943294fa5ddb.herokuapp.com/',
+        image: 'https://raw.githubusercontent.com/cdziedzic/React-Portfolio/main/src/assets/accolade.png',
+        technology: 'React, MongoDb, GraphQL, Express, Node.js',
+        description: 'An application to track video games played, create custom achievements and monitor completion'
+
+    },
+    {
         title: 'Votewave',
         github: 'https://github.com/brianmyer/votewave',
         image: 'https://github.com/cdziedzic/Weather-dashboard/raw/main/assets/image.png',
-        technology: 'JavaScript, node,js, SQL, handlebars, tailwind',
+        technology: 'JavaScript, Node,js, SQL, handlebars, tailwind',
         description: 'An app to quickly and easily create an online poll'
     },
+  
     {
         title: 'Weather Dashboard',
         github: 'https://github.com/cdziedzic/Weather-dashboard',
         deployed: 'https://cdziedzic.github.io/Weather-dashboard/',
         image: 'https://github.com/cdziedzic/Weather-dashboard/raw/main/assets/image.png',
-        technology: 'JavaScript, day.js, bootstrap'
-    },
-    {
-        title: 'Logo Creator',
-        github: 'https://github.com/cdziedzic/Logo-creator',
-        image: ''
+        technology: 'JavaScript, day.js, bootstrap',
+        description: 'A web app to check the weather and display a 5-day forecast'
     },
     {
         title: 'Personal Calendar',
         github: 'https://github.com/cdziedzic/Personal-Calendar',
         deployed: 'https://cdziedzic.github.io/Personal-Calendar/',
-        image: 'https://github.com/cdziedzic/Personal-Calendar/raw/main/image.png'
+        image: 'https://github.com/cdziedzic/Personal-Calendar/raw/main/image.png',
+        description: 'A basic daily planner app. Create and save to-do items in time slots which chaange color based on the time',
+        technology: 'JavaScript, Node.js'
         
     },
     {
         title: 'Note pad',
         github: 'https://github.com/cdziedzic/Note-pad',
-        image: 'https://github.com/cdziedzic/Note-pad/raw/main/image.png'
+        image: 'https://github.com/cdziedzic/Note-pad/raw/main/image.png',
+        technology: '',
+        deployed: ''
     }
 ]
 
